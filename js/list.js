648f24a296fe6d38f3634144ed90091547c79b44
Michael LaRosa <mikecl1041@gmail.com>
2017-12-10 12:40:06 -0800

    New options

--- js/list.js
+++ js/list.js
@@ -9,4 +9,20 @@ document.querySelector("#add-item").addEventListener("click", function(){
 	document.querySelector("#my-list").appendChild(newItem);
 });
 
+document.querySelector("#my-list").addEventListener("click", function(i) {
+	if(i.target.className.indexOf("delete") > 0) {
+		var li = i.target.parentElement;
+		li.parentElement.removeChild(li);
+	}
+});
+
+document.querySelector("#search-item").addEventListener("keyup", function(){
+	document.querySelectorAll(".list-group-item").forEach(function(li) {
+		if(li.firstElementChild.innerText.indexOf(document.querySelector("#search-item").value) < 0) {
+			li.style.display = "none";
+		} else {
+			li.style.display = "initial";
+		}
+	})
+});
 
