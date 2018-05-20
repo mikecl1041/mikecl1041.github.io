1e15f73d233df94c2809d710d4a2db9aa2713177
Michael LaRosa <mikecl1041@gmail.com>
2018-05-20 12:08:15 -0700

    New page

---
+++ js/forum.js
@@ -0,0 +1,34 @@
+
+var d = new Date();
+
+var postHold;
+
+var Post = function(name, text, link) {
+	this.name = name
+	this.date = d.getMonth()+1 + "/" + d.getDate() + "/" + d.getFullYear()
+	this.text = text
+	this.link = link
+	this.likes = 0
+}
+
+/*document.querySelector("#submit").addEventListener("click", function() {
+	event.preventDefault();
+	var name = document.querySelector("#name").value;
+	var link = document.querySelector("#link").value;
+	var text = document.querySelector("#text").value;
+	postHold = new Post(name, text, link);
+});*/
+
+document.querySelector('#submit').addEventListener("click", loadDoc());
+
+function loadDoc() {
+  var xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+      document.getElementById("demo").innerHTML =
+      this.responseText;
+    }
+  };
+  xhttp.open("GET", "colors.txt", true);
+  xhttp.send();
+}
