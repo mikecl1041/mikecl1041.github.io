254e5c2ea54b330c888a1f0a9246a5ae135e3072
Michael LaRosa <mikecl1041@gmail.com>
2018-05-23 17:07:29 -0700

    Firestore!

--- js/forum.js
+++ js/forum.js
@@ -1,37 +1,62 @@
 
-var d = new Date();
-
-var postHold;
-
-var Post = function(name, text, link) {
-	this.name = name
-	this.date = d.getMonth()+1 + "/" + d.getDate() + "/" + d.getFullYear()
-	this.text = text
-	this.link = link
-	this.likes = 0
-}
-
-/*document.querySelector("#submit").addEventListener("click", function() {
-	event.preventDefault();
-	var name = document.querySelector("#name").value;
-	var link = document.querySelector("#link").value;
-	var text = document.querySelector("#text").value;
-	postHold = new Post(name, text, link);
-});*/
-
 document.querySelector('#submit').addEventListener("click", function() {
 	event.preventDefault();
 	loadDoc();
 });
 
+var db = firebase.firestore();
+var ref = db.collection('Forum Content');
+var info = [];
+
 function loadDoc() {
-  var xhttp = new XMLHttpRequest();
-  xhttp.onreadystatechange = function() {
-    if (this.readyState == 4 && this.status == 200) {
-      document.getElementById("demo").innerHTML =
-      this.responseText;
-    }
-  };
-  xhttp.open("GET", "colors.txt", true);
-  xhttp.send();
+	db.collection("Forum Content").add({
+		name: document.querySelector('#name').value,
+		date: new Date(),
+		text: document.querySelector('#text').value,
+		link: document.querySelector('#link').value,
+		likes: 0
+	})
+	.then(function(docRef) {
+		console.log("Document written with ID: ", docRef.id);
+	})
+	.catch(function(error) {
+		console.error("Error adding document: ", error);
+	});
 }
+
+function query() {
+	ref.get().then(function(querySnapshot) {
+		querySnapshot.docs.map(function (documentSnapshot,x) {
+			info.push(documentSnapshot.data());
+			info[x].key = documentSnapshot.id;
+		});
+	});
+}
+
+function populate() {
+	for (i=0; i<info.length; i++) {
+		document.getElementById("post-container").appendChild(document.createElement("li")).className = "post list-group-item";
+		document.querySelectorAll('.post')[i].appendChild(document.createElement("span")).className = "name text-muted small";
+		document.querySelectorAll('.post')[i].appendChild(document.createElement("br"))
+		document.querySelectorAll('.post')[i].appendChild(document.createElement("span")).className = "date text-muted small";
+		document.querySelectorAll('.post')[i].appendChild(document.createElement("span")).className = "float-right likes";
+		document.querySelectorAll('.post')[i].appendChild(document.createElement("br"))
+		document.querySelectorAll('.post')[i].appendChild(document.createElement("a")).className = "link";
+		document.querySelectorAll('.post')[i].querySelector('.link').href = "https://" + info[i].link;
+		document.querySelectorAll('.post')[i].querySelector('.link').target = "_blank";
+		document.querySelectorAll('.post')[i].appendChild(document.createElement("br"))
+		document.querySelectorAll('.post')[i].appendChild(document.createElement("span")).className = "text-entered";
+		document.querySelectorAll('.post')[i].querySelector('.name').innerHTML = info[i].name;
+		document.querySelectorAll('.post')[i].querySelector('.date').innerHTML = info[i].date;
+		document.querySelectorAll('.post')[i].querySelector('.text-entered').innerHTML = info[i].text;
+		document.querySelectorAll('.post')[i].querySelector('.link').innerHTML = info[i].link;
+		document.querySelectorAll('.post')[i].querySelector('.likes').innerHTML = info[i].likes;
+	}
+}
+
+document.getElementById("update").addEventListener("click", function() {
+	query();
+	populate();
+});
+
+
